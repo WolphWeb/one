9a78b049fccdcc5d32eaa7f3780f64a831addb27
WolphWeb <52222178+WolphWeb@users.noreply.github.com>
2019-06-26 00:37:56 +0200

    EP1

---
+++ Wolph Web - EP1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/resetstyle.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <title>Wolph Web - EP1</title>
+</head>
+<body>
+<div id="section1"></div>
+<div id="section2">
+    <h1 data-aos="flip-right">Hello, world!</h1>
+</div>
+</body>
+<script src="js/jquery.js"></script>
+<script src="js/aos.js"></script>
+<script>
+AOS.init({
+
+    duration: 1500,
+
+});
+</script>
+</html>
